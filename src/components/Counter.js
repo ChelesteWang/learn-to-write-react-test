6e98cb8783da1f682da7b7091dd6ea6d80f91591
ChelesteWang <1581446178@qq.com>
2022-04-17 16:43:30 +0800

    feat: 编写计数器组件

---
+++ src/components/Counter.js
@@ -0,0 +1,23 @@
+import React, { useRef } from "react";
+
+function Counter(props) {
+  const initialProps = useRef(props).current;
+  const { initialCount = 0, maxClicks = 3 } = props;
+
+  const [count, setCount] = React.useState(initialCount);
+  const tooMany = count >= maxClicks;
+
+  const handleReset = () => setCount(initialProps.initialCount);
+  const handleClick = () => setCount((currentCount) => currentCount + 1);
+
+  return (
+    <div>
+      <button onClick={handleClick} disabled={tooMany}>
+        Count: {count}
+      </button>
+      {tooMany ? <button onClick={handleReset}>reset</button> : null}
+    </div>
+  );
+}
+
+export { Counter };
